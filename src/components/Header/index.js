2fe135b5ce98f5b2d9126f146a3b5a942c12021c
Eduardoamaral66 <eduardoamaral66@gmail.com>
2020-04-05 15:02:23 -0300

    feat: add drop card into another card

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { GiPegasus, GiLibertyWing } from "react-icons/gi";
 
 import { Container } from "./styles";
 
 export default function Header() {
   return (
     <Container>
-      <h1>Rocketfy</h1>
+      <h1>
+        <GiPegasus /> Pegasus
+      </h1>
     </Container>
   );
 }
